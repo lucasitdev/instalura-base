b494740be2bc670fa1abf09593d821998f0fc1f1
Lucas Assuncao da Silva <lucas.itengineer@outlook.com>
2021-03-16 14:59:54 -0300

    aula 07 módulo 01

--- src/components/commons/Button/index.js
+++ src/components/commons/Button/index.js
@@ -1,39 +1,39 @@
 import styled, { css } from 'styled-components';
 import get from 'lodash/get';
-
 import { TextStyleVariantsMap } from '../../foundation/Text';
 import { breakpointsMedia } from '../../../theme/utils/breakpointsMedia';
+import { propToStyle } from '../../../theme/utils/propToStyle';
 
-const buttonGhost = css`
-    color: ${({ theme, variant }) => get(theme, `colors.${variant}.color`)};
-    background: transparent;
+const ButtonGhost = css`
+  color: ${({ theme, variant }) => get(theme, `colors.${variant}.color`)};
+  background-color: transparent;
 `;
-
-const buttonDefault = css`
-    color: ${({ theme, variant  }) => get(theme, `colors.${variant}.contrastText`)};
-    background-color: ${({ theme, variant }) => get(theme, `colors.${variant}.color`)};
+const ButtonDefault = css`
+  color: ${({ theme, variant }) => get(theme, `colors.${variant}.contrastText`)};
+  background-color: ${({ theme, variant }) => get(theme, `colors.${variant}.color`)};
 `;
-
 export const Button = styled.button`
-    border: 0;
-    cursor: pointer;
-    padding: 12px 26px;
-    font-weight: bold;
-    opacity: 1;
-    border-radius: ${({ theme }) => theme.borderRadius};
-    ${TextStyleVariantsMap.smallestException}
-    ${({ ghost }) => ghost ? buttonGhost : buttonDefault}
-    transition: opacity ${({ theme }) => theme.transition};
-    &:hover,
-    &:focus {
-        opacity: .5;
-    }
-    ${breakpointsMedia({
-        xs: css`
-            ${TextStyleVariantsMap.smallestException}
-        `,
-        md: css`
-            ${TextStyleVariantsMap.paragraph1}
-        `,
-    })}
+  border: 0;
+  cursor: pointer;
+  padding: 12px 26px;
+  font-weight: bold;
+  opacity: 1;
+  transition: opacity ${({ theme }) => theme.transition};
+  border-radius: ${({ theme }) => theme.borderRadius};
+  ${breakpointsMedia({
+    xs: css`
+      ${TextStyleVariantsMap.smallestException}
+    `,
+    md: css`
+      padding: 12px 43px;
+      ${TextStyleVariantsMap.paragraph1}
+    `,
+  })}
+  ${propToStyle('margin')}
+  ${propToStyle('display')}
+  ${({ ghost }) => (ghost ? ButtonGhost : ButtonDefault)}
+  &:hover,
+  &:focus {
+    opacity: .5;
+  }
 `;
--- src/components/foundation/Text/index.js
+++ src/components/foundation/Text/index.js
@@ -1,23 +1,51 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
 import PropTypes from 'prop-types';
+import { get } from 'lodash';
+import { propToStyle } from '../../../theme/utils/propToStyle';
+import { breakpointsMedia } from '../../../theme/utils/breakpointsMedia';
+
+const smallestException = css`
+  ${({ theme }) => css`
+    font-size: ${theme.typographyVariants.smallestException.fontSize};
+    font-weight: ${theme.typographyVariants.smallestException.fontWeight};
+    line-height: ${theme.typographyVariants.smallestException.lineHeight};
+  `}
+`;
+
+const paragraph1 = css`
+  ${({ theme }) => css`
+    font-size: ${theme.typographyVariants.paragraph1.fontSize};
+    font-weight: ${theme.typographyVariants.paragraph1.fontWeight};
+    line-height: ${theme.typographyVariants.paragraph1.lineHeight};
+  `}
+`;
 
 export const TextStyleVariantsMap = {
-    paragraph1: css`
-        font-size: ${({ theme }) => theme.typographyVariants.paragraph1.fontSize};
-        font-weight: ${({ theme }) => theme.typographyVariants.paragraph1.fontWeight};
-        line-height: ${({ theme }) => theme.typographyVariants.paragraph1.lineHeight};
-    `,
-
-    smallestException: css`
-        font-size: ${({ theme }) => theme.typographyVariants.smallestException.fontSize};
-        font-weight: ${({ theme }) => theme.typographyVariants.smallestException.fontWeight};
-        line-height: ${({ theme }) => theme.typographyVariants.smallestException.lineHeight};
-    `,
+    smallestException,
+    paragraph1,
+    title: css`
+    ${({ theme }) => css`
+        font-size: ${theme.typographyVariants.titleXS.fontSize};
+        font-weight: ${theme.typographyVariants.titleXS.fontWeight};
+        line-height: ${theme.typographyVariants.titleXS.lineHeight};
+    `}
+    ${breakpointsMedia({
+        md: css`
+            ${({ theme }) => css`
+                font-size: ${theme.typographyVariants.title.fontSize};
+                font-weight: ${theme.typographyVariants.title.fontWeight};
+                line-height: ${theme.typographyVariants.title.lineHeight};
+        `}        
+        `,
+    })}
+    `,      
 };
 
 const TextBase = styled.span`
     ${({ variant }) => TextStyleVariantsMap[variant]}
+    color: ${({ theme, color }) => get(theme, `colors.${color}.color`)};
+    ${propToStyle('textAlign')}
 `;
 
 export default function Text({ tag, variant, children }) {
@@ -32,9 +60,9 @@ export default function Text({ tag, variant, children }) {
 }
 
 Text.propTypes = {
-    tag: PropTypes.string,
-    variant: PropTypes.string,
+    tag: PropTypes.oneOf(['h1', 'h2', 'h3', 'h4', 'h5', 'p', 'li', 'a', 'span']),   
     children: PropTypes.node.isRequired,
+    variant: PropTypes.oneOf(['title', 'paragraph1', 'smallestException']),
 };
 
 Text.defaultProps = {
